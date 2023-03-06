cdd52ba04f131b8b681a8f8edd5a779c7d770563
Anthony Rafael Arteaga Guaman <arteaga.anthonyp2021@gmail.com>
2023-03-06 16:59:30 -0500

    Añadi Metas con html

--- mesapp/src/App.jsx
+++ mesapp/src/App.jsx
@@ -1,17 +1,18 @@
 
 import React from "react";
-import { useState } from 'react'
 import "./App.css";
 import Encabezado from './componentes/compartidos/Encabezado'
 import Principal from './componentes/compartidos/Principal'
 import Pie from './componentes/compartidos/Pie'
+import Meta from "./componentes/lista/meta";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
         <Encabezado></Encabezado>
-        <Principal></Principal>
+        <Principal>
+          <Meta></Meta>
+        </Principal>
         <Pie></Pie>
       
     </div>
